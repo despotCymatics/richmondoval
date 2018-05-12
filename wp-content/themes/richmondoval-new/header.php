1d022cf5d9d302389c38bc46d2330e79786c3534
despot <despot.cymatics@gmail.com>
2018-05-12 22:31:20 +0200

    correct

--- wp-content/themes/richmondoval-new/header.php
+++ wp-content/themes/richmondoval-new/header.php
@@ -53,14 +53,28 @@
     })(window,document,'script','dataLayer','GTM-NLCDPL');</script>
     <!-- End Google Tag Manager -->
 
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KW4BQQK');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body <?php body_class(); ?> style="overflow: hidden;">
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLCDPL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLCDPL"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KW4BQQK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <div id="loading-wrap">
         <img title="logo-animate" src="<?=get_template_directory_uri()?>/images/basic/logo-animate.gif">
