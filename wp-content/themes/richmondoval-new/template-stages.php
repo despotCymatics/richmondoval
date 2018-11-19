4c356f8ba5270d4ffc1bde0e2745c005d60d6550
despot <despot.cymatics@gmail.com>
2018-11-19 23:05:39 +0100

    26hrs correct

--- wp-content/themes/richmondoval-new/template-stages.php
+++ wp-content/themes/richmondoval-new/template-stages.php
@@ -280,12 +280,13 @@ if ( login() || isset( $_SESSION['logged'] ) ) {
 	                                $diff = date_diff( $now, $sessionDateTime);
 	                                $diffHours = $diff->h+$diff->days*24;
 
-	                                if( $diffHours > 26) $disableClass = 'disableBook';
+	                                if( $diffHours >= 26) $disableClass = 'disableBook';
 
                                     $sessionDate = date("D, M jS", strtotime($session->StartDateTime));
                                     $sessionTime = date("g:ia", strtotime($session->StartDateTime))." - ".date("g:ia", strtotime('+'.$session->Duration.' minutes',strtotime($session->StartDateTime)));
 
                                     ?>
+
                                     <div class="showMoreToggler <?=$disableClass;?>"
                                          data-user-id="<?=$userId; ?>"
                                          data-session-id="<?=$session->Id; ?>"
