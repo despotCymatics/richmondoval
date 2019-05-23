b4464935c558c512e1e80354ab0b560c6367b42b
despotCymatics <despot.cymatics@gmail.com>
2019-05-23 12:26:31 +0200

    my ga

--- wp-content/themes/richmondoval-new/header.php
+++ wp-content/themes/richmondoval-new/header.php
@@ -28,39 +28,50 @@
 
 	<?php wp_head();?>
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-68610770-1', 'auto');
-        ga('send', 'pageview');
+      ga('create', 'UA-68610770-1', 'auto');
+      ga('send', 'pageview');
 
-    </script>
+  </script>
 
-<!--    <script type="text/javascript">
-    (function(d, s, id) {
-      window.Wishpond = window.Wishpond || {};
-      Wishpond.merchantId = '1298220';
-      Wishpond.writeKey = '45a34a9a1d39';
-      var js, wpjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//cdn.wishpond.net/connect.js";
-      wpjs.parentNode.insertBefore(js, wpjs);
-    }(document, 'script', 'wishpond-connect'));
-    </script>-->
+  <!--<script type="text/javascript">
+  (function(d, s, id) {
+    window.Wishpond = window.Wishpond || {};
+    Wishpond.merchantId = '1298220';
+    Wishpond.writeKey = '45a34a9a1d39';
+    var js, wpjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//cdn.wishpond.net/connect.js";
+    wpjs.parentNode.insertBefore(js, wpjs);
+  }(document, 'script', 'wishpond-connect'));
+  </script>-->
 
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KW4BQQK');</script>
-    <!-- End Google Tag Manager -->
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KW4BQQK');</script>
+  <!-- End Google Tag Manager -->
 
-	<?php if(is_page_template("template-stages.php") || is_page_template("template-stages_login.php") || is_page_template("template-stages_registration.php")) { ?>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21048776-4"></script>
+  <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-21048776-4');
+  </script>
+
+  <?php if(is_page_template("template-stages.php") || is_page_template("template-stages_login.php") || is_page_template("template-stages_registration.php")) { ?>
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134995895-1"></script>
         <script>
